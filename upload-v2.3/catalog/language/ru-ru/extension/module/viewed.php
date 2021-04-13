082f4acf92d32906acef0daa52687b0384e07269
Victor <victor8730@ukr.net>
2021-04-13 21:58:04 +0300

    add module for oc 2.3

---
+++ upload-v2.3/catalog/language/ru-ru/extension/module/viewed.php
@@ -0,0 +1,8 @@
+<?php
+// Heading
+$_['heading_title'] = 'Самые просматриваемые';
+
+// Text
+$_['text_tax']      = 'Без НДС:';
+$_['button_cart']      = 'Купить';
+$_['button_wishlist']      = 'В закладки';
